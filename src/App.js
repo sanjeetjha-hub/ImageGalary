04c9a7a5bfb214904514a7f7f306645c0b797e72
sanjeetjha-hub <1999sanjeetjha@gmail.com>
2023-06-25 16:26:23 +0530

    Implemented the galary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Card from "./components/Card";
 
+const photos = [
+  "https://picsum.photos/id/1001/200/200",
+  "https://picsum.photos/id/1002/200/200",
+  "https://picsum.photos/id/1003/200/200",
+  "https://picsum.photos/id/1004/200/200",
+  "https://picsum.photos/id/1005/200/200",
+  "https://picsum.photos/id/1006/200/200",
+  "https://picsum.photos/id/1009/200/200",
+  "https://picsum.photos/id/1008/200/200",
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container text-center mt-5">
+        <h1 className="mb-5">Gallery</h1>
+        <div className="row">
+          {photos.map((photo) => (
+            <Card src={photo} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
